b1666b42aeaa719650eeb5e9ac182e9f1fa5f1b5
Alessandro Mammana <amammana@google.com>
2020-03-06 18:28:59 +0100

    Styling change for homepage

--- myshop-app/src/index.js
+++ myshop-app/src/index.js
@@ -72,7 +72,9 @@ class ShopApp extends React.Component {
     return (
       <div className="shop-app">
         <h1>Alessandro's store</h1>
-        <a href="/">Homepage</a>
+        <p>
+          <a href="/">Homepage</a>
+        </p>
         <p>
           In this store you are going to find beautiful images that you can buy
         </p>
